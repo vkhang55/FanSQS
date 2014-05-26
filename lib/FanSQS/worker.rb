49dd0adc337a4f8e2479d2e50ed89441a706b025
Khang Pham <vkhang55@gmail.com>
2014-05-26 03:36:43 -0700

    modified worker class

--- lib/FanSQS/worker.rb
+++ lib/FanSQS/worker.rb
@@ -2,16 +2,24 @@ module FanSQS
   module Worker
     def self.included(base)
       base.extend(ClassMethods)
-      base.class_attribute :fan_sqs_options
+      base.class_attribute :fan_sqs_options_hash
     end
 
     module ClassMethods
       attr_accessor :queue
 
       def perform_async(options = {})
-        name = fan_sqs_options ? fan_sqs_options[:queue] : :default
+        name = fan_sqs_options_hash ? fan_sqs_options_hash[:queue] : :default
         queue = FanSQS::Queue.instantiate(name)
         queue.send_message(options.to_json)
+      end
+
+      def set_fan_sqs_options(options = {})
+        if options.empty?
+          self.fan_sqs_options_hash = { queue: :default }
+        else
+          self.fan_sqs_options_hash = options
+        end
       end
 
       def enqueue(*msg_body)
