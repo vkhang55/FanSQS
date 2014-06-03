c441d591d334667c1be479e63141c90222b0fa0f
Khang Pham <vkhang55@gmail.com>
2014-06-02 22:08:35 -0700

    cleaned up Queue Wrapper

--- lib/FanSQS/queue_wrapper.rb
+++ lib/FanSQS/queue_wrapper.rb
@@ -3,10 +3,6 @@ module FanSQS
     @@cache ||= {} # cache for queues
 
     class << self
-      def create_message(klass, msg_body)
-        instantiate(klass.queue).send_message({ class: klass.name, body: msg_body }.to_json)
-      end
-
       # Find out if the queue exists. If it does not exist, create a new one
       # This line below has a high chance of causing confusion as it will try to use either FanSQS::Queue or AWS::SQS::Queue. Maybe use the class from AWS gem instead?
       def instantiate(qname)
